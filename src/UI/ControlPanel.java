f4c84b2eeb571f696448e5ed9b772f234498d491
Eric McAlpine <emcalpine144@gmail.com>
2016-06-08 09:44:20 -0400

    Update ControlPanel.java
    Added some basic essentials + references to needed information.

--- src/UI/ControlPanel.java
+++ src/UI/ControlPanel.java
@@ -2,7 +2,7 @@
     ControlPanel.java
     created 4-20-2016
     
-    purpose: Panel that 
+    purpose: Panel that houses all the controls used for interacting with the ooms.
 */
 
 import javax.swing.*;
@@ -10,12 +10,27 @@ import javax.swing.*;
 public class ControlPanel extends JPanel{
 
 	// Variable Dictionary 
-	//JButton
-	//JLabel
-	//JTextArea
+	JButton btn_playPause, btn_speed;
+	JLabel lbl_statusLabel;
+	JTextBox txt_speed;
 	
 	public ControlPanel()
 	{
+		// Initialize objects
+		btn_playPause = new JButton();
+		btn_speed = new JButton();
+		
+		lbl_statusLabel = new JLabel("Status Label");
+		
+		txt_speed = new JTextBox();
+		
+		// Add action listeners
+		// NOTE: May need to add status label reference to Menu_AL instantiation,
+		//       so that it knows where to output.
+		
+		// Add/organize objects to panel
+		// currently NO CLUE how to do this. 
+		//CelsiusConverterGUI.java tutorial on Oracle's page might help.
 		
 	}
 }
