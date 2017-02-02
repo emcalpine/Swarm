323a121a911c12e1738680751273f0b24b7f536f
Eric McAlpine <emcalpine144@gmail.com>
2017-02-02 09:34:29 -0500

    Update Location.java
    Added direction, so we can draw it more easily.

--- src/Engine/stuff/Location.java
+++ src/Engine/stuff/Location.java
@@ -3,36 +3,59 @@ package stuff;
     Location.java
     created 4-20-2016
     
-    purpose: Class that represents a location, currently a pair of coordinates
+    purpose: Class that represents a location, given as a pair of coordinates,
+             and an orientation.
     
 */
 
 
 public class Location {
 	
+	// A set of Cartesian coordinates.
 	double x = 0.0;
 	double y = 0.0;
+	
+	// The orientation, given in degrees from positive x
+	double direction = 0.0;
 	
 	public Location()
 	{
 		
 	}
 	
-	public Location(double i, double j)
+	public Location(double i, double j, double d)
 	{
 		x = (i > 0.0) ? i : x;
 		y = (j > 0.0) ? j : y;
+		direction = d % 360.0; 
 	}
 	
 	public Location(Location pos)
 	{
 		x = pos.x;
 		y = pos.y;
+		direction = pos.direction;
 	}
 	
-	public void update(double i, double j)
+	public void update(double i, double j, double d)
 	{
 		x = (i > 0.0) ? i : x;
 		y = (j > 0.0) ? j : y;
+		direction = d % 360.0;
+	}
+	
+	public double getX()
+	{
+		return x;
+	}
+	
+	public double getY()
+	{
+		return y;
+	}
+	
+	public double getDirection()
+	{
+		return direction;
 	}
 }
